efc982c71482475d6645a1813df2d50877add030
gitgitwi <johnwi@knou.ac.kr>
2020-07-24 15:34:04 +0900

    init project

---
+++ src/test/java/com/art/springmuseum/SpringMuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.springmuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
